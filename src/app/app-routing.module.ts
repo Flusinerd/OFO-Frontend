0b97f01ad171d233ca674b1664d486ff096bc1f4
Tom Nissing <tom.l.nissing@gmail.com>
2020-04-14 18:41:42 +0200

    generated homePage

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.page';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes),
+    HomeComponent
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
