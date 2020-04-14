a8bc25228e32573b6337ad3264626a089f972f09
Jan Krüger <jankrueger1999@gmail.com>
2020-04-14 20:43:07 +0200

    Updated App-Title

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,10 +20,10 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it('should have as title \'ofo-app\'', () => {
+  it('should have as title \'OFO\'', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('ofo-app');
+    expect(app.title).toEqual('OFO');
   });
 
   it('should render title', () => {
