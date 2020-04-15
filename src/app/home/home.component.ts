20c05c211dbf7bb4e831c34b3b66bfeadbb4a32b
Tom Nissing <tom.l.nissing@gmail.com>
2020-04-15 12:51:57 +0200

    added animation to home

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { inOutAnimation } from '../animations/slide';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
+  animations: [inOutAnimation('300ms')],
 })
 export class HomeComponent implements OnInit {
 
