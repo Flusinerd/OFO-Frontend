503e95e3627f1609e884a65dec6c7fdda1ab699d
Jan Krüger <jankrueger1999@gmail.com>
2020-04-25 23:51:43 +0200

    Fixed linting

--- src/app/animations/slide.ts
+++ src/app/animations/slide.ts
@@ -1,19 +1,11 @@
 import { transition, style, animate, trigger, AnimationTriggerMetadata } from '@angular/animations';
 
 export function inOutAnimation(time: string): AnimationTriggerMetadata {
-    return trigger('inOutAnimation', [
-      transition(
-        ':enter',
-        [
-          style({opacity: 0, transform: "translateX(200px) scale(0.8)"}),
-          animate(time + ' 200ms ease-out', style({opacity: 1, transform: "translateX(0) scale(1)"}))
-        ]
-      ),
-      transition(
-        ':leave',
-        [
-          animate(time + ' ease-in', style({opacity: 0, transform: "translateX(-200px) scale(0.8)"}))
-        ]
-      )
-    ]);
-  }
+  return trigger('inOutAnimation', [
+    transition(':enter', [
+      style({ opacity: 0, transform: 'translateX(200px) scale(0.8)' }),
+      animate(time + ' 200ms ease-out', style({ opacity: 1, transform: 'translateX(0) scale(1)' }))
+    ]),
+    transition(':leave', [animate(time + ' ease-in', style({ opacity: 0, transform: 'translateX(-200px) scale(0.8)' }))])
+  ]);
+}
